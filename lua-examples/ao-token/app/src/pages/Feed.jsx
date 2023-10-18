9b18526eeb1c469084014fd5f8d361a6814fba84
Justin Shaw <mogulx_operates@yahoo.com>
2023-10-18 10:40:22 -0500

    refactor: add some styling

--- lua-examples/ao-token/app/src/pages/Feed.jsx
+++ lua-examples/ao-token/app/src/pages/Feed.jsx
@@ -11,6 +11,8 @@ function Feed({ goToPlayer }) {
   const signer = createDataItemSigner(globalThis.arweaveWallet);
   const [interactionId, setInteractionId] = useState();
   const [processState, setProcessState] = useState();
+  const [amount, setAmount] = useState(0);
+  const [recipient, setRecipient] = useState("");
 
   useEffect(() => {
     readState({
@@ -18,21 +20,68 @@ function Feed({ goToPlayer }) {
     })
       .then(setProcessState)
       .catch((e) => console.log(e));
-  }, []);
+  }, [interactionId]);
 
   return (
-    <>
-      <h1>Process TX: {import.meta.env.VITE_PROCESS_ID}</h1>
+    <div className="max-w-2xl mx-auto p-4">
+      <h1 className="text-xl font-bold mb-4">
+        Process TX: {import.meta.env.VITE_PROCESS_ID}
+      </h1>
       <h1
         onClick={() => goToPlayer("<player name>")}
-        className="text-3xl font-bold underline"
+        className="text-3xl font-bold underline cursor-pointer mb-4"
       >
         Process State:
       </h1>
       <p>{JSON.stringify(processState)}</p>
 
+      <div className="mb-4">
+        <label className="block mb-2">
+          Amount:
+          <input
+            className="border border-gray-300 p-2 w-full"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </label>
+
+        <label className="block mb-2">
+          Recipient:
+          <input
+            className="border border-gray-300 p-2 w-full"
+            type="text"
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+        </label>
+
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+          onClick={async () => {
+            if (globalThis.arweaveWallet) {
+              await globalThis.arweaveWallet.connect(["SIGN_TRANSACTION"]);
+            }
+            const interactionId = await writeInteraction({
+              contractId: import.meta.env.VITE_PROCESS_ID,
+              input: {
+                function: "transfer",
+                target: recipient,
+                qty: Math.floor(amount * 1e6),
+              },
+              signer,
+              tags: [],
+            });
+            setInteractionId(interactionId);
+          }}
+        >
+          Transfer
+        </button>
+      </div>
+
       <div>
         <button
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
           onClick={async () => {
             if (globalThis.arweaveWallet) {
               await globalThis.arweaveWallet.connect(["SIGN_TRANSACTION"]);
@@ -49,9 +98,11 @@ function Feed({ goToPlayer }) {
           Mint 1
         </button>
 
-        {interactionId && <h3>Ineraction ID: {interactionId}</h3>}
+        {interactionId && (
+          <h3 className="mt-4">Interaction ID: {interactionId}</h3>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
